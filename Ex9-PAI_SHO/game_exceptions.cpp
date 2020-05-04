1f3e517203b7d62b10a93600881937df4eb0719a
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-05-04 15:52:07 +0300

    add cpp and header files of exceptions that can throw through the game

---
+++ Ex9-PAI_SHO/game_exceptions.cpp
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: game_exceptions.cpp
+Author: Liri (3/5/2020)
+Purpose: This file contain class of exceptions that inherits from
+			exception class, and functions that
+			throw the errors
+\********************************************************/
---
+++ Ex9-PAI_SHO/game_exceptions.h
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: game_exceptions.h
+Author: Liri (3/5/2020)
+Purpose: This header file contain class of exceptions that inherits from 
+			exception class, and declaration of functions that 
+			throw the errors
+\********************************************************/
