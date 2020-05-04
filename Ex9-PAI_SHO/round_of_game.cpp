311ad37b09fb6f45c4e2177fd723dbddfd9e1437
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-05-04 11:06:55 +0300

    add header file and cpp file of functions that play one round of game

---
+++ Ex9-PAI_SHO/round_of_game.cpp
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: round_of_game.cpp
+Author: Liri (3/5/2020)
+Purpose: This file contain class of Round_Of_Game, inherits from
+			Player, that contain functions that play
+			one round in the game
+\********************************************************/
---
+++ Ex9-PAI_SHO/round_of_game.h
@@ -0,0 +1,8 @@
+/********************************************************\
+File Name: round_of_game.h
+Author: Liri (3/5/2020)
+Purpose: This header file contain class of Round_Of_Game, inherits from
+			Player, that contain declaration of functions that play
+			one round in the game
+\********************************************************/
+
