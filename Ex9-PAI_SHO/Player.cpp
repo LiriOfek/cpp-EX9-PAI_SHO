3ac4ee2098a382b3553fbf0a5101f7595a87328a
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-05-04 08:51:23 +0300

    add header file and cpp file of Player class with documentation

---
+++ Ex9-PAI_SHO/Player.cpp
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: Player.h
+Author: Liri (3/5/2020)
+Purpose: This header file contain functions of Player,
+			that roll die and get player score
+\********************************************************/
---
+++ Ex9-PAI_SHO/Player.h
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: Player.h
+Author: Liri (3/5/2020)
+Purpose: This header file contain class of Player, with declaration of
+			functions that roll die and get player score
+\********************************************************/
