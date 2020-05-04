5942d34ce49db62086567b0d98a0425992d1b18d
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-05-04 08:58:52 +0300

    add constructor to class Player that initiate the random number generator, and set the score of the player to 0

--- Ex9-PAI_SHO/Player.cpp
+++ Ex9-PAI_SHO/Player.cpp
@@ -1,6 +1,13 @@
 /********************************************************\
-File Name: Player.h
+File Name: Player.cpp
 Author: Liri (3/5/2020)
 Purpose: This header file contain functions of Player,
 			that roll die and get player score
 \********************************************************/
+
+#include "Player.h"
+
+Player::Player() {
+		srand(static_cast<unsigned int>(time(0)));
+		score = 0;
+}
--- Ex9-PAI_SHO/Player.h
+++ Ex9-PAI_SHO/Player.h
@@ -4,3 +4,22 @@ Author: Liri (3/5/2020)
 Purpose: This header file contain class of Player, with declaration of
 			functions that roll die and get player score
 \********************************************************/
+
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+class Player {
+	int score;
+public:
+	Player();
+	/**
+	* @brief  create Player, initiate random number generator,
+	*			and set the player score to 0
+	* @return this function has no return value
+	* @note   create Player, and set its score to zero
+	* @author  Liri
+	*/
+};
