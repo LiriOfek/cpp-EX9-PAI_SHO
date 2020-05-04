2448f97ae34d7c5d2daaaea2388842afefefe80a
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-05-04 11:24:09 +0300

    add cpp and header file of end_of_game with documentation

---
+++ Ex9-PAI_SHO/end_of_game.cpp
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: end_of_game.cpp
+Author: Liri (3/5/2020)
+Purpose: This header file contain functions of class of End_Of_Game, 
+			inherits from Round_Of_Game, that implemenet
+			the logics of end of game
+\********************************************************/
---
+++ Ex9-PAI_SHO/end_of_game.h
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: end_of_game.h
+Author: Liri (3/5/2020)
+Purpose: This header file contain class of End_Of_Game, inherits from
+			Round_Of_Game, with declaration of functions that implemenet
+			the logics of end of game
+\********************************************************/
