91774981c6ac59744536cd19e38a3da161874248
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-05-04 16:51:21 +0300

    add exception struct for not using treated die, that return the same result

--- Ex9-PAI_SHO/game_exceptions.h
+++ Ex9-PAI_SHO/game_exceptions.h
@@ -24,3 +24,17 @@ struct Exception_Not_Use_Regulare_Die : public exception {
 		return "Error - You can use only one regulare die with 6 sides!";
 	}
 };
+
+struct Exception_Same_Result_Of_Die : public exception {
+	/**
+	* @brief  throw exception for using treated die, because during 8 turns 
+	*			there was the same result of the dice
+	* @return const char* that explain the user can't use treated die
+	*			which return the same result 
+	* @note   throw exception
+	* @author  Liri
+	*/
+	const char * what() const throw () {
+		return "Error - During 8 turns the result of your die was the same, therefore you are using treated die!";
+	}
+};
