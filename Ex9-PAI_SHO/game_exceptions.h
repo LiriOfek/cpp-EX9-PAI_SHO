30bc7c72e88bdf560d6a243acae4de728a859033
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-05-04 15:56:44 +0300

    add function class that throw exception if the player not use regulare die

--- Ex9-PAI_SHO/game_exceptions.h
+++ Ex9-PAI_SHO/game_exceptions.h
@@ -1,7 +1,19 @@
 /********************************************************\
 File Name: game_exceptions.h
 Author: Liri (3/5/2020)
-Purpose: This header file contain class of exceptions that inherits from 
-			exception class, and declaration of functions that 
+Purpose: This file contain class of exceptions that inherits from 
+			exception class, and functions that 
 			throw the errors
 \********************************************************/
+
+#pragma once
+
+#include <iostream>
+#include <exception>
+using namespace std;
+
+struct Exception_Not_Use_Regulare_Die : public exception {
+	const char * what() const throw () {
+		return "Error - You can use only one regulare die with 6 sides!";
+	}
+};
