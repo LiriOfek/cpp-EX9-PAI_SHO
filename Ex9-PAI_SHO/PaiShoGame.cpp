9b5cf43bd5c58be9a343ed566da6599104c9d8a0
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-05-04 12:30:44 +0300

    add function that set the interface of the end of the game according to the choise of the user

--- Ex9-PAI_SHO/PaiShoGame.cpp
+++ Ex9-PAI_SHO/PaiShoGame.cpp
@@ -11,3 +11,23 @@ Purpose: This file contain class of PaiShoGame, that inherits from
 void PaiShoGame::play() {
 	play_till_difference_of_25_points(player1, player2);
 }
+
+void PaiShoGame::set_interface_end_of_game() {
+	int interface_of_end;
+	std::cout << "How do you want the game to end?" << std::endl
+		  	  << "Type 0 - The game is oven after 10 turns, and the player with the lowest score wins" << std::endl
+			  << "Type 1 - The game is over when one of the players has more than 152 points" << std::endl
+			  << "Type 2 - The game is over after one player has 25 points or more than the other player" << std::endl;
+	std::cin >> interface_of_end;
+	switch (interface_of_end) {
+	case(0):
+		interface_end_of_game = LOWEST_SCORE;
+		break;
+	case(1):
+		interface_end_of_game = MORE_THAN_152_POINTS;
+		break;
+	case(2):
+		interface_end_of_game = DIFFERENCE_OF_25;
+		break;
+	}
+}
--- Ex9-PAI_SHO/PaiShoGame.h
+++ Ex9-PAI_SHO/PaiShoGame.h
@@ -10,10 +10,27 @@ Purpose: This header file contain class of PaiShoGame, that inherits from
 #include "Player.h"
 #include "end_of_game.h"
 
+enum END_OF_GAME { LOWEST_SCORE, MORE_THAN_152_POINTS, DIFFERENCE_OF_25 };
+
 class PaiShoGame :public End_Of_Game {
 	Player player1;
 	Player player2;
+	END_OF_GAME interface_end_of_game;
 public:
 	void play();
-
+	/**
+	* @brief  play Pai Sho game - call function that play the game accorfing
+	*			to the interface of the end of the game
+	* @return this function has no return value
+	* @author  Liri
+	*/
+	void set_interface_end_of_game();
+	/**
+	* @brief  set the interface of the end of the game according to the choise
+	*			of the user
+	* @return this function has no return value
+	* @notes  this function change interface_end_of_game according to the 
+	*			choise of the user
+	* @author  Liri
+	*/
 };
