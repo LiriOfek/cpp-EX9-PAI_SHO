4fb28fa1a968465fcdab5d60c0b0a0720b7bbb29
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-05-04 10:10:39 +0300

    add main file with documentation

---
+++ Ex9-PAI_SHO/main.cpp
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: main.cpp
+Author: Liri (3/5/2020)
+Purpose: This main file create object of Pai Sho game 
+			and run the function that play Pai Sho game 
+\********************************************************/
