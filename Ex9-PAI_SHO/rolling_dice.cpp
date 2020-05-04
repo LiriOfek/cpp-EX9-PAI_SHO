34a59746ba2b415829df9dbf361c605fda83aefa
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-05-04 13:51:58 +0300

    add cpp and header files that contain functions for rolling dice

---
+++ Ex9-PAI_SHO/rolling_dice.cpp
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: rolling_dice.cpp
+Author: Liri (3/5/2020)
+Purpose: This file contain class of Rolling_Dice,
+			with functions that implemenet
+			options of rolling dice
+\********************************************************/
---
+++ Ex9-PAI_SHO/rolling_dice.h
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: rolling_dice.h
+Author: Liri (3/5/2020)
+Purpose: This header file contain class of Rolling_Dice, 
+			with declaration of functions that implemenet
+			options of rolling dice
+\********************************************************/
